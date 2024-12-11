c34e9378cce098386341282d18e13817a1070118
Itay <itayargov@mail.tau.ac.il>
2024-12-11 10:39:32 +0000

    refactor to recursive part 2

--- 2024/day11/part2.ts
+++ 2024/day11/part2.ts
@@ -1,45 +1,28 @@
 import { sumOf } from "@std/collections/sum-of";
 import { readInput } from "./input.ts";
-import { blinkStone, type Stone } from "./part1.ts";
+import { memoize } from "@std/cache";
 
-type StoneLine = Map<Stone, number>;
+const powOf10Halfed = (l: number) => Math.pow(10, Math.floor(l / 2));
 
-const dp = new Map<Stone, Stone[]>();
+const countStones = memoize((stone: number, d: number): number => {
+  if (d === 0) return 1;
+  if (stone === 0) return countStones(1, d - 1);
 
-function simulateBlinks(stones: StoneLine, n: number) {
-  for (let i = 0; i < n; i++) {
-    const newStoneLine = new Map<Stone, number>();
-    for (const [key, value] of stones.entries()) {
-      if (!dp.has(key)) {
-        dp.set(key, blinkStone(key));
-      }
-
-      const explodesInto = dp.get(key)!;
-      for (const explode of explodesInto) {
-        newStoneLine.set(explode, (newStoneLine.get(explode) || 0) + value);
-      }
-    }
-
-    stones = newStoneLine;
+  const l = Math.floor(Math.log10(stone)) + 1;
+  if (l % 2 === 0) {
+    const half = powOf10Halfed(l);
+    return (
+      countStones(Math.floor(stone / half), d - 1) +
+      countStones(Math.floor(stone % half), d - 1)
+    );
   }
 
-  return stones;
-}
-
-function initStoneLine(stones: Stone[]): StoneLine {
-  const stoneLine = new Map<Stone, number>();
-  for (const stone of stones) {
-    stoneLine.set(stone, (stoneLine.get(stone) || 0) + 1);
-  }
-
-  return stoneLine;
-}
+  return countStones(stone * 2024, d - 1);
+});
 
 function part2(input: string) {
-  const stones = input.split(" ");
-  const stoneLine = initStoneLine(stones);
-  const simulatedStones = simulateBlinks(stoneLine, 75);
-  console.log(sumOf(simulatedStones.values(), (x) => x));
+  const stones = input.split(" ").map(Number);
+  console.log(sumOf(stones, (s) => countStones(s, 75)));
 }
 
 if (import.meta.main) {
