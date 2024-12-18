7e2ac9623a9051ab823759725f7b8cc7e0f1d302
Itay <itayargov@mail.tau.ac.il>
2024-12-18 10:20:59 +0000

    refactor day 18 part2 to binary search

--- 2024/day18/part2.ts
+++ 2024/day18/part2.ts
@@ -6,21 +6,49 @@ import {
   getGrid,
 } from "./part1.ts";
 
+function binarySearch(l: number, r: number, f: (i: number) => boolean) {
+  while (l < r) {
+    const m = Math.floor((l + r) / 2);
+    if (f(m)) {
+      l = m + 1;
+    } else {
+      r = m;
+    }
+  }
+  return l;
+}
+
+function turnOffUntil(grid: boolean[][], bytes: [number, number][], r: number) {
+  for (let i = 0; i <= r; i++) {
+    const [c, r] = bytes[i];
+    grid[r][c] = false;
+  }
+  for (let i = r + 1; i < bytes.length; i++) {
+    const [c, r] = bytes[i];
+    grid[r][c] = true;
+  }
+}
+
+function checkIfGridAccessible(
+  grid: boolean[][],
+  bytes: [number, number][],
+  r: number,
+) {
+  turnOffUntil(grid, bytes, r);
+  const visited = bfs(grid, 0, 0);
+  if (visited.has(END_LOC.join(","))) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function part2() {
   const bytes = getBytesFalling();
   const grid = getGrid(bytes.slice(0, BYTES_BOUND));
-  let i = 0;
-  while (true) {
-    const newBound = i + BYTES_BOUND;
-    const [c, r] = bytes[newBound];
-    grid[r][c] = false;
-    const visited = bfs(grid, 0, 0);
-    if (!visited.has(END_LOC.join(","))) {
-      console.log([c, r].join(","));
-      break;
-    }
-    i++;
-  }
+  const [l, r] = [BYTES_BOUND, bytes.length - 1];
+  const res = binarySearch(l, r, (r) => checkIfGridAccessible(grid, bytes, r));
+  console.log(res, bytes[res]);
 }
 
 if (import.meta.main) {
